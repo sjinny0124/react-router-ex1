276cbbe0f7f30dc907202178441bf0db5a85fc63
sjinny0124 <sjinny0124@gmail.com>
2019-04-15 14:41:57 +0900

    url 쿼리 파라메타

--- src/index.js
+++ src/index.js
@@ -3,21 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import'./App.css';
 
-const Home = () => <h1>Home</h1>;
-const About = () => <h1>About</h1>;
-
-const isActiveLink = (match, location) => {
-    console.log(match, location, location.search);
-
-    if( location.search && /number=(\d+)/.test(location.search)) return true;
-    return false;
-};
-
 const Links = () => (
     <nav>
-        <NavLink replace to="/about/">about</NavLink>
-        <NavLink activeStyle={{ color: 'red' }} replace to={{ pathname: '/about'}}>About</NavLink>
-        <NavLink isActive={isActiveLink} activeClassName="active" replace to="/contact?number=123">child</NavLink>
+        <Link to="/?id=123">Inlineblock</Link>
+        <Link to={{ pathname: '/', search: 'id=456&x=1'}}>Object</Link>
     </nav>
 );
 
@@ -25,35 +14,13 @@ const App = () => (
 <Router>
     <div>
         <Links />
-        <Route exact path="/:page/:subpage" render={({match}) => {
-            console.log(match);
-            return (
-            <div>
-                <h1>page: {match.params.page}</h1>
-                <h1>subpage: {match.params.subpage}</h1>
-            </div>
-            );
-        }}
+        <Route path="/" render={({match, location}) => (
+        <div>
+            <div>{JSON.stringify(match)}</div>
+            <div>{JSON.stringify(location)}</div>
+        </div>
+        )}
         />
-        <Route path="/:date(\d{4}-\d{2}-\d{2}).:ext(\w+)" render={({match}) => {
-            console.log(match);
-            return (
-            <div>
-                <h1>date: {match.params.date}</h1>
-                <h1>ext: {match.params.ext}</h1>
-            </div>
-            );
-        }}
-         />
-         <Route path="/child" children={(prop) => {
-             console.log('차일드',prop);
-             if( prop.match ) {
-                return <div>차일드</div>;    
-             }
-
-             return null;
-         }} 
-         />
     </div>
 </Router>
 );
