bc7ea1bf58115cc977a794942ae22c3548a48a35
sjinny0124 <sjinny0124@gmail.com>
2019-04-15 16:20:14 +0900

    Prompt 예제 커밋

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import {
   Route,
   Link,
   Switch,
-  Redirect
+  Redirect,
+  Prompt
 } from "react-router-dom";
 import "./App.css";
 
@@ -13,40 +14,34 @@ const isLoggedIn = false;
 const Links = () => (
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/protected">인증된 사람만 볼 수 있다.</Link>
+    <Link to="/form">From</Link>
   </nav>
 );
 
-const SiginIn = () => (
-  <div>
-    <h2>로그인</h2>
-    <form>
-      <input placeholder="ID" type="text" />
-      <input placeholder="PW" type="password" />
-    </form>
-  </div>
-);
+class Form extends React.Component {
+  state = { dirty: false };
+  setDirty = () => this.setState({ dirty: true });
+  render() {
+    return (
+      <div>
+        <h1>Form {this.state.dirty ? "작성중" : ""}</h1>
+        <input type="text" onInput={this.setDirty} />
+        <Prompt
+          when={this.state.dirty}
+          message={"저장되지 않은 데이터가 있습니다. 정말 이동할까요?"}
+        />
+      </div>
+    );
+  }
+}
 
 const ProtectedPage = () => <h2>인증된 페이지</h2>;
 const App = () => (
   <Router>
     <div>
       <Links />
-      <Switch>
-        <Route exact path="/" render={() => <h1>Home</h1>} />
-        <Route
-          path="/signin"
-          render={() =>
-            isLoggedIn ? <Redirect to="/protected" /> : <SiginIn />
-          }
-        />
-        <Route
-          path="/protected"
-          render={() =>
-            isLoggedIn ? <ProtectedPage /> : <Redirect to="/signin" />
-          }
-        />
-      </Switch>
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route path="/form" component={Form} />
     </div>
   </Router>
 );
