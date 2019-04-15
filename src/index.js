0bd7fe499abc50f88c1292c548a55c3ba5d3ea5e
sjinny0124 <sjinny0124@gmail.com>
2019-04-15 16:01:46 +0900

    로그인 예제 커밋

--- src/index.js
+++ src/index.js
@@ -9,14 +9,25 @@ import {
 } from "react-router-dom";
 import "./App.css";
 
+const isLoggedIn = false;
 const Links = () => (
   <nav>
     <Link to="/">Home</Link>
-    <Link to="/old">Old</Link>
-    <Link to="/new">New</Link>
+    <Link to="/protected">인증된 사람만 볼 수 있다.</Link>
   </nav>
 );
 
+const SiginIn = () => (
+  <div>
+    <h2>로그인</h2>
+    <form>
+      <input placeholder="ID" type="text" />
+      <input placeholder="PW" type="password" />
+    </form>
+  </div>
+);
+
+const ProtectedPage = () => <h2>인증된 페이지</h2>;
 const App = () => (
   <Router>
     <div>
@@ -24,12 +35,16 @@ const App = () => (
       <Switch>
         <Route exact path="/" render={() => <h1>Home</h1>} />
         <Route
-          path="/new/:id"
-          render={({ match }) => <h1>New: {match.params.id}</h1>}
+          path="/signin"
+          render={() =>
+            isLoggedIn ? <Redirect to="/protected" /> : <SiginIn />
+          }
         />
         <Route
-          path="/old/:id"
-          render={({ match }) => <Redirect to={`/new/${match.params.id}`} />}
+          path="/protected"
+          render={() =>
+            isLoggedIn ? <ProtectedPage /> : <Redirect to="/signin" />
+          }
         />
       </Switch>
     </div>
