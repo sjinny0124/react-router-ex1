506424fe393d34d83d805b703afedf27d7d0fb14
sjinny0124 <sjinny0124@gmail.com>
2019-04-16 10:50:48 +0900

    hoc 예제 커밋

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TabApp from "./examples/TabApp";
 import "./App.css";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
-const routes = (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/home" component={TabApp} />
-      <Redirect from="/" to="/home" />
-    </Switch>
-  </BrowserRouter>
+const User = ({ name }) => <div className="User">{name}</div>;
+
+const hoc = BaseComponent => props => {
+  console.log("===>", props, BaseComponent);
+  return <BaseComponent {...props} name="Jim" />;
+};
+
+const hoc2 = overrideProps => BaseComponent => props => (
+  <BaseComponent {...props} {...overrideProps} />
 );
-ReactDOM.render(routes, document.getElementById("root"));
+
+const User3 = hoc2({ name: "Bob" })(User);
+const User2 = hoc(User);
+
+const App = () => (
+  <div>
+    <User name="Tim" />
+    <User2 name="Tim" />
+    <User3 name="Tim" />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
