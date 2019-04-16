14d8f0e91e9b87801c9901f9eaa8951405287114
sjinny0124 <sjinny0124@gmail.com>
2019-04-16 11:02:07 +0900

    hoc 예제 커밋

--- src/index.js
+++ src/index.js
@@ -12,15 +12,41 @@ const hoc2 = overrideProps => BaseComponent => props => (
   <BaseComponent {...props} {...overrideProps} />
 );
 
+const hoc3 = BaseComponent =>
+  class extends React.Component {
+    shouldComponentUpdate() {
+      return true; //false: 업데이트 되더라도 변경되지 않음
+    }
+    render() {
+      return <BaseComponent {...this.props} />;
+    }
+  };
+
 const User3 = hoc2({ name: "Bob" })(User);
+const User4 = hoc3(User);
 const User2 = hoc(User);
 
-const App = () => (
-  <div>
-    <User name="Tim" />
-    <User2 name="Tim" />
-    <User3 name="Tim" />
-  </div>
-);
+class App extends React.Component {
+  state = {
+    name: "Kim"
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ name: "김" });
+    }, 5000);
+  }
+
+  render() {
+    return (
+      <div>
+        <User name="Tim" />
+        <User2 name="Tim" />
+        <User3 name="Tim" />
+        <User4 name={this.state.name} />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
